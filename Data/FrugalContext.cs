da4a38c9960d245871aba0424e15a372bd8d8be8
Timothy Flowers <t_chuck_flowers@yahoo.com>
2020-02-26 21:41:56 -0500

    Creates the DbContext including the budget DbSet

---
+++ Data/FrugalContext.cs
@@ -0,0 +1,22 @@
+using Frugal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Frugal.Data
+{
+    /// <summary>
+    /// The database context used by the Frugal application
+    /// </summary>
+    public class FrugalContext : DbContext
+    {
+        /// <summary>
+        /// Constructs a database context with the supplied options.
+        /// </summary>
+        /// <param name="options">The options used to configure the context.</param>
+        public FrugalContext(DbContextOptions options) : base(options) { }
+
+        /// <summary>
+        /// The database set of Budget objects within the database.
+        /// </summary>
+        DbSet<Budget> Budgets { get; set; } = null!;
+    }
+}
